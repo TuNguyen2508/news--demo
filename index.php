def0c783eb55e3630eba19f878bef5f359f2040d
TuNguyen2508 <22211tt4282@mail.tdc.edu.vn>
2024-10-31 13:35:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+include "slider.php";
+?>
+<!-- Featured News Slider Start -->
+<?php include "featured-news.php" ?>
+<!-- Featured News Slider End -->
+
+<!-- Lastest News -->
+<?php
+include "news-sidebar.php";
+?>
+<!-- Lastest News End -->
+<!-- Footer Start -->
+<?php
+include "footer.php";
+?>
